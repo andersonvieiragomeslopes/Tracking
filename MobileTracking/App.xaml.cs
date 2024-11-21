9241687863c1277a0ac61c3d90535199c55d2e2e
Anderson Lopes <andersonvieira123456@gmail.com>
2024-11-21 19:35:20 -0300

    Adicionar arquivos de projeto.

---
+++ MobileTracking/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MobileTracking;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new AppShell();
+    }
+}
---
+++ MobileTracking/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MobileTracking;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
