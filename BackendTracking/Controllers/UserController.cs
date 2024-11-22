521c89c36a56443b645dda95dce7034b4b54fab3
Anderson Lopes <andersonvieira123456@gmail.com>
2024-11-22 12:48:18 -0300

    users api and database sqlite

---
+++ BackendTracking/Controllers/UserController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Shared.Responses;
+using System.Text.Json;
+using Tracking.BusinessLogicLayer.Blls;
+
+namespace BackendTracking.Controllers
+{
+    [ApiController]
+    [Route("api/")]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserBLL _userBLL;
+        public UserController(IUserBLL userBLL)                 
+        {
+            _userBLL = userBLL;
+        }
+        //[Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        [HttpPost("v1/users")]
+        public async Task<ActionResult> GetUsers()
+        {
+            try
+            {
+                var users = await _userBLL.GetAll();
+                return Ok(users);
+            }
+            catch (KeyNotFoundException error)
+            {
+                return NotFound(new Response<string>(StatusCodes.Status404NotFound,
+                    JsonSerializer.Serialize(new
+                    {
+                        error.Message,
+                        error.Source
+                    })));
+            }
+            catch (DbUpdateException error)
+            {
+                return UnprocessableEntity(new Response<string>(StatusCodes.Status422UnprocessableEntity,
+                    JsonSerializer.Serialize(new
+                    {
+                        error.Message,
+                        error.Source
+                    })));
+            }
+            catch (Exception error)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                    new Response<string>(StatusCodes.Status500InternalServerError,
+                JsonSerializer.Serialize(new
+                {
+                    error.Message,
+                    error.Source
+                })));
+            }
+        }
+    }
+}
--- BackendTracking/Program.cs
+++ BackendTracking/Program.cs
@@ -1,3 +1,5 @@
+using Tracking.IoC;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,6 +9,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+DependenceInjector.Register(builder.Services, builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
