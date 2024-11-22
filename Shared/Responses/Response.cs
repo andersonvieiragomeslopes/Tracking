0f24b4c61ac6dc348ef2d5470610e11522a9b84f
Anderson Lopes <andersonvieira123456@gmail.com>
2024-11-22 12:48:34 -0300

    add responses

---
+++ Shared/Responses/Response.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Responses
+{
+    public class Response<T>
+    {
+        public Response(int httpStatusCode, T data, int code)
+        {
+            HttpStatusCode = httpStatusCode;
+            Data = data;
+            Code = code;
+
+        }
+        public Response(int httpStatusCode, T data)
+        {
+            HttpStatusCode = httpStatusCode;
+            Data = data;
+        }
+
+        public int Code { get; set; }
+        public T Data { get; set; }
+        public int HttpStatusCode { get; set; }
+    }
+}
