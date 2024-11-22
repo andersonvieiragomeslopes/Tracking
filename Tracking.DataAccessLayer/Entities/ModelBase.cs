7516497be6230840601364150c8e0d07ecbb09b8
Anderson Lopes <andersonvieira123456@gmail.com>
2024-11-22 12:49:35 -0300

    add entities user and order

---
+++ Tracking.DataAccessLayer/Entities/ModelBase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tracking.DataAccessLayer.Entities
+{
+    public abstract class ModelBase
+    {
+        public virtual Guid Id { get; set; }
+        public virtual DateTime CreatedAt { get; set; }
+        public virtual DateTime UpdatedAt { get; set; }
+    }
+}
---
+++ Tracking.DataAccessLayer/Entities/Order.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tracking.DataAccessLayer.Entities
+{
+    public class Order : ModelBase
+    {
+        //vai ter endereço, userId e mais informações.
+    }
+}
