ce57567ef0397150a2d610ee177ff7151daa3a2a
Anderson Lopes <andersonvieira123456@gmail.com>
2024-11-22 12:46:53 -0300

    add mappers

---
+++ Tracking.DataAccessLayer/Mappers/BaseEntityMapper.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tracking.DataAccessLayer.Entities;
+
+namespace Tracking.DataAccessLayer.Mappers
+{
+    public abstract class BaseEntityMapper<TEntity> : IEntityTypeConfiguration<TEntity>
+          where TEntity : ModelBase
+    {
+        public void Configure(EntityTypeBuilder<TEntity> builder)
+        {
+            builder.Property(c => c.CreatedAt)
+                .HasColumnName("CreatedAt")
+                .IsRequired();
+
+            builder.Property(c => c.UpdatedAt)
+                .HasColumnName("UpdatedAt")
+                .IsRequired();
+
+            ConfigureBuilder(builder);
+        }
+
+        public abstract void ConfigureBuilder(EntityTypeBuilder<TEntity> builder);
+    }
+}
---
+++ Tracking.DataAccessLayer/Mappers/UserEntityMapper.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tracking.DataAccessLayer.Entities;
+
+namespace Tracking.DataAccessLayer.Mappers
+{
+    public class UserEntityMapper : BaseEntityMapper<User>
+    {
+        public override void ConfigureBuilder(EntityTypeBuilder<User> builder)
+        {
+            builder.ToTable("Users");
+            builder.Property(x => x.Name)
+                .HasColumnName("Name")
+                .HasMaxLength(50)
+                .IsRequired();
+            builder.HasIndex(x => x.Name, "name")
+                .IsUnique();
+        }
+    }
+}
