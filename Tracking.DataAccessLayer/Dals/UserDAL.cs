211075483ae4e8d5f0732da5fd83f1068588c073
Anderson Lopes <andersonvieira123456@gmail.com>
2024-11-22 12:50:18 -0300

    add user dal

---
+++ Tracking.DataAccessLayer/Dals/UserDAL.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tracking.DataAccessLayer.Entities;
+
+namespace Tracking.DataAccessLayer.Dals
+{
+    public interface IUserDAL
+    {
+        Task<int> CountAsync();
+        Task<IEnumerable<User>> GetAll();
+    }
+    public class UserDAL : IUserDAL
+    {
+        private readonly TrackingContext _context;
+        public UserDAL(TrackingContext context)
+        {
+            _context = context;
+        }
+        public async Task<int> CountAsync()
+        {
+            return await _context.Users
+                .CountAsync();
+        }
+        public async Task<IEnumerable<User>> GetAll()
+        {
+            try
+            {
+                return await _context.Users
+                    .ToListAsync();
+            }
+            catch (DbUpdateException error)
+            {
+                throw new DbUpdateException(error.Message);
+            }           
+            catch (Exception error)
+            {
+                throw new Exception("An error occurred.", error);
+            }
+        }
+    }
+}
---
+++ Tracking.DataAccessLayer/TrackingContext.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+using Tracking.DataAccessLayer.Entities;
+using Tracking.DataAccessLayer.Mappers;
+
+namespace Tracking.DataAccessLayer
+{
+    public class TrackingContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        protected readonly IConfiguration Configuration;
+        public TrackingContext(DbContextOptions<TrackingContext> options, IConfiguration configuration) : base(options)
+        {
+            Configuration = configuration;
+            Database.EnsureCreatedAsync().Wait();
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfiguration(new UserEntityMapper());
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
+        }
+        public async override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            string createdAt = "CreatedAt";
+            string updatedAt = "UpdatedAt";
+            PropertyEntry propertyCreatedAt;
+            PropertyEntry propertyUpdatedAt;
+            var entries = ChangeTracker.Entries();
+
+            foreach (var entry in entries)
+            {
+                try
+                {
+                    propertyCreatedAt = entry.Property(createdAt);
+                    propertyUpdatedAt = entry.Property(updatedAt);
+                }
+                catch (Exception ex)
+                {
+                    propertyCreatedAt = null;
+                    continue;
+                }
+
+                if (propertyCreatedAt != null && entry.State == EntityState.Added)
+                {
+                    if ((DateTime)propertyCreatedAt.CurrentValue == default)
+                    {
+                        propertyCreatedAt.CurrentValue = DateTime.UtcNow;
+                        propertyUpdatedAt.CurrentValue = DateTime.UtcNow;
+                    }
+                }
+
+                try
+                {
+                    propertyUpdatedAt = entry.Property(updatedAt);
+                }
+                catch (Exception ex)
+                {
+                    propertyUpdatedAt = null;
+                    continue;
+                }
+
+                if (propertyUpdatedAt != null && (entry.State == EntityState.Added
+                    || entry.State == EntityState.Modified))
+                {
+                    entry.Property(updatedAt).CurrentValue = DateTime.Now;
+                }
+            }
+
+            return await base.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
