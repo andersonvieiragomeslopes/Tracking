61dbbdb5395d4f36d1a8715d074c6260c31b3fb2
Anderson Lopes <andersonvieira123456@gmail.com>
2024-11-22 12:49:59 -0300

    add IoC container

---
+++ Tracking.IoC/DependenceInjector.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tracking.BusinessLogicLayer.Blls;
+using Tracking.DataAccessLayer;
+using Tracking.DataAccessLayer.Dals;
+
+namespace Tracking.IoC
+{
+    public class DependenceInjector
+    {
+        public static void Register(IServiceCollection services, IConfigurationManager configuration)
+        {
+            services.AddDbContext<TrackingContext>();
+            services.AddScoped<IUserDAL, UserDAL>();
+            services.AddScoped<IUserBLL, UserBLL>();
+        }
+    }
+}
