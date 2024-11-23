a055a53caae59b7365c5cdcf5e2f233ce65756ca
Anderson Lopes <andersonvieira123456@gmail.com>
2024-11-22 23:44:31 -0300

    fluentvalidation

--- MobileTracking/ViewModels/LoginViewModel.cs
+++ MobileTracking/ViewModels/LoginViewModel.cs
@@ -1,5 +1,8 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using FluentValidation;
+using FluentValidation.Results;
+using Shared.Mobile;
 using Shared.Mobile.Services;
 using System;
 using System.Collections.Generic;
@@ -14,26 +17,67 @@ namespace MobileTracking.ViewModels
         private readonly IApiRequestService _apiRequestService;
 
         [ObservableProperty]
-        public Guid? _id = null;
-
+        public Guid? id = null;
+        [ObservableProperty]
+        private ValidationResult _validationResult = new ValidationResult();
 
         public LoginViewModel(IApiRequestService apiRequestService)
         {
-            _apiRequestService = apiRequestService;
+            _apiRequestService = apiRequestService;           
+        }
+        public override Task InitializeAsync(object navigationData)
+        {
+            return base.InitializeAsync(navigationData);
+        }
+        protected override Task InitializeAsync()
+        {
+            return base.InitializeAsync();
         }
         [RelayCommand]
         public async Task Login()
         {
-            if (Id.HasValue)
+            var validator = new LoginPageViewModelValidator();
+
+            this.ValidationResult = await validator.ValidateAsync(this);
+            if (!ValidationResult.IsValid)
             {
-                var response = await _apiRequestService.AuthAsync(Id.Value);
+                return;
             }
+
+            var response = await _apiRequestService.AuthAsync(Id.Value);
+                if(response.Token != null)
+                await SecureStorage.SetAsync(Constants.AccessToken, response.Token);
+            
 
         }
         [RelayCommand]
         public async Task Generate()
         {
+          
+
             Id = await _apiRequestService.GenerateUserAsync();
         }
+        //https://medium.com/@chausse.nicolas/input-validation-in-net-maui-with-fluentvalidation-and-syncfusion-toolkit-ab8e7fc05e2d
+        public class LoginPageViewModelValidator : AbstractValidator<LoginViewModel>
+        {
+            public static string IdProperty => nameof(Id);
+            public static string GlobalProperty => "Global";
+
+            public LoginPageViewModelValidator()
+            {
+
+                RuleFor(x => x).Custom((model, context) =>
+                {
+                    var guidIdValid = Guid.TryParse(model.Id.ToString(), out var id);
+                    if (guidIdValid)
+                    {
+                        return;
+                    }
+
+                    context.AddFailure(GlobalProperty, "UserId invalid or not found.");
+                });
+            }
+        }
     }
+
 }
