090e343466204d02b107c439f26ec9075b928b69
Anderson Lopes <andersonvieira123456@gmail.com>
2024-11-23 14:59:08 -0300

    add pins on map project.

--- MobileTracking/ViewModels/HomeViewModel.cs
+++ MobileTracking/ViewModels/HomeViewModel.cs
@@ -1,6 +1,8 @@
-﻿using MobileTracking.Services;
+﻿using Maui.GoogleMaps;
+using MobileTracking.Services;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,14 +12,22 @@ namespace MobileTracking.ViewModels
     public partial class HomeViewModel : BaseViewModel
     {
         private readonly IInitalizeBackgroundService _initalizeBackgroundService;
+
+        public ObservableCollection<Pin> Pins { get; set; }
+
         public HomeViewModel(INavigationService navigationService, IInitalizeBackgroundService initalizeBackgroundService) : base(navigationService)
         {
             _initalizeBackgroundService = initalizeBackgroundService;
         }
-        public override Task InitializeAsync(object navigationData)
+        public async override Task InitializeAsync(object navigationData)
         {
             _initalizeBackgroundService.StartSyncOrders();
-            return base.InitializeAsync(navigationData);
+            foreach (var pin in await _apiRequestService.MyOrdersAsync()) 
+            {
+                if(Pins != null) 
+                Pins.Add(new Pin() { Label = pin.Title, Address = pin.Title, Position = new Position(pin.Latitude, pin.Longitude)});
+            }
+
         }
     }
 }
