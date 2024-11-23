f6a86c31bdf8e3280d7806b833b7d8e7eaee5c9f
Anderson Lopes <andersonvieira123456@gmail.com>
2024-11-22 21:05:54 -0300

    Bearer authorization added

--- Tracking.BusinessLogicLayer/Blls/AuthBLL.cs
+++ Tracking.BusinessLogicLayer/Blls/AuthBLL.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
 using Shared;
 using System;
@@ -9,6 +10,7 @@ using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 using Tracking.BusinessLogicLayer.DTOs;
+using Tracking.BusinessLogicLayer.DTOs.ViewModels;
 using Tracking.DataAccessLayer.Entities;
 
 namespace Tracking.BusinessLogicLayer.Blls
@@ -16,15 +18,19 @@ namespace Tracking.BusinessLogicLayer.Blls
     public interface IAuthBLL
     {
         Task<TokenDTO> Login(Guid id);
+        Task<UserViewModel> Logged();
     }
     public class AuthBLL : IAuthBLL
     {
         private readonly IUserBLL _userBLL;
         private readonly IConfiguration _configuration;
-        public AuthBLL(IUserBLL userBLL, IConfiguration configuration)
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        public AuthBLL(IUserBLL userBLL, IConfiguration configuration, IHttpContextAccessor httpContextAccessor)
         {
             _userBLL = userBLL;
             _configuration = configuration;
+            _httpContextAccessor = httpContextAccessor;
+
         }
 
         public async Task<TokenDTO> Login(Guid id)
@@ -32,8 +38,29 @@ namespace Tracking.BusinessLogicLayer.Blls
             var response = await _userBLL.Get(id);
             if (response == null)
                 throw new Exception("User not found");
-            return BuildToken(new User { Id = response.Id});
+            return BuildToken(new User { Id = response.Id });
 
+        }
+        public async Task<UserViewModel> Logged()
+        {
+
+            if (_httpContextAccessor.HttpContext.User?.Identity?.IsAuthenticated ?? false)
+            {
+                var isGuid = Guid.TryParse(_httpContextAccessor.HttpContext.User.Identity.Name, out var id);
+                if (isGuid)
+                {
+                    return await _userBLL.Get(id);
+                }
+                //if (_httpContextAccessor.HttpContext.User.Identity.Name is Guid id) 
+                //{
+
+                //}
+                //var user = Guid._httpContextAccessor.HttpContext.User.Identity.Name.ToString()
+
+                // _httpContextAccessor.HttpContext.User.Identity.Name
+                //    ?? "";
+            }
+            throw new NullReferenceException("Unauthenticated user");
         }
         private TokenDTO BuildToken(User user)
         {
